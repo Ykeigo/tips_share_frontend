4b1f7690f4377ab64555710f2438c74695ae2db7
Ykeigo <yanagirts@gmail.com>
2023-02-26 07:54:20 +0000

    変数処理追加

--- react-typescript/src/MyTips/MyTips.tsx
+++ react-typescript/src/MyTips/MyTips.tsx
@@ -1,22 +1,21 @@
-import React, { useState } from "react";
+import { useEffect, useContext } from "react";
 import "./MyTips.css";
 import { FaRegLightbulb } from "react-icons/fa";
+import { MyTipsContext, MyTip } from "./MyTipsProvider";
 import TipsHeplfulButton from "./TipsHelpfulButton";
-import tipsData from "./my-tips-data.json";
-import {Tip, MyTip} from "../types"
 
 export default function MyTips() {
-  const [myTips, setMyTips] = useState<Array<MyTip>>(tipsData.MyTips);
+  const { myTips } = useContext(MyTipsContext);
 
   return (
-    <div className="MyTips">
-      <div>お役に立ちましたか？</div>
-      {myTips.map((tip) => MyTip(tip,setMyTips))}
-    </div>
+      <div className="MyTips">
+        <div>お役に立ちましたか？</div>
+        {myTips.map((tip) => MyTipField(tip))}
+      </div>
   );
 }
 
-function MyTip(myTip: MyTip, setMyTips) {
+function MyTipField(myTip: MyTip) {
   const items = [];
   for (let i = 0; i < myTip.helpful_num; i++) {
     items.push(<FaRegLightbulb fontSize={20}></FaRegLightbulb>);
@@ -25,22 +24,9 @@ function MyTip(myTip: MyTip, setMyTips) {
   return (
     <div className="MyTip">
       <div className="title">title = {myTip.tip.title}</div>
-    <div className="HelpfulLevelBar">
-      {HelpfulButton(myTip,setMyTips)}
-      <div className="helpful">{items}</div>
-    </div>
+      {TipsHeplfulButton(myTip.id)}
+      {items}
     </div>
   );
 }
 
-function HelpfulButton(myTip:MyTip, setMyTips) {
-  
-
-  const updateMyTip = (toUpdate:MyTip) => setMyTips(
-    myTips.map((myTip) => (myTip.id === toUpdate.id ? { ...toUpdate } : myTip))
-  );
-
-  return (
-    {TipsHeplfulButton(myTip, )}
-  );
-}
--- react-typescript/src/MyTips/TipsHelpfulButton.tsx
+++ react-typescript/src/MyTips/TipsHelpfulButton.tsx
@@ -1,22 +1,28 @@
-import React from "react";
-import { MyTip } from "../types";
+import React, { useContext } from "react";
+import { MyTipsContext, MyTip } from "./MyTipsProvider";
 
-export default function TipsHeplfulButton(myTip: MyTip, updateTip:(updated:MyTip) => void) {
+export default function TipsHeplfulButton(id: string) {
+  const { getTip, updateTip } = useContext(MyTipsContext);
+
   const submit = () => {
     //e.preventDefault();
 
-    const updated = {
-      ...myTip,
-      helpful_today: !myTip.helped_today,
-      helpful_num: myTip.helpful_num + 1,
-    };
-
-    updateTip(updated);
+    const current = getTip(id);
+    if (current) {
+      console.log(current.helped_today);
+      const numDiff = current.helped_today ? -1 : 1;
+      const updated = {
+        ...current,
+        helped_today: !current.helped_today,
+        helpful_num: current.helpful_num + numDiff,
+      };
+      updateTip(updated);
+    }
   };
 
   return (
     <div className="TipsHelpfulButton">
-      <button onClick={submit}>参考になった！</button>
+      <button onClick={submit}>{getTip(id)?.helped_today ? "取り消し" : "役立った"}</button>
     </div>
   );
 }
